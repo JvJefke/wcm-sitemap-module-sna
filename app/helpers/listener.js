d8f1b265b67668406d61e629224b85c070d4b623
Jeroen Valcke <valcke_jeroen@hotmail.com>
2018-12-06 10:21:08 +0100

    remove async await since it wil be run in Node 6.x

--- app/helpers/listener.js
+++ app/helpers/listener.js
@@ -18,18 +18,14 @@ const getLocations = (contentItem) => R.compose(
 	R.pathOr(null, ["meta", "slug"])
 )(contentItem)
 
-const isPage = (content) => {
-	const ct = await R.composeP(
-		R.ifElse(
-			R.type("String"),
-			getContentType,
-			R.prop("uuid")
-		),
-		R.path(["meta", "contentType"])
-	)(content);
-
-	return R.path(["meta", "canBeFiltered"])(ct);
-};
+const isPage = (content) => R.composeP(
+	R.ifElse(
+		R.type("String"),
+		getContentType,
+		R.prop("uuid")
+	),
+	R.path(["meta", "contentType"])
+)(content).then((ct) => R.path(["meta", "canBeFiltered"])(ct));
 
 const generateSitemapObjects = () => {
 	const lastmod = R.pathOr(new Date().toISOString(), ["meta", "lastModified"])(content);
@@ -40,7 +36,7 @@ const generateSitemapObjects = () => {
 	)(content);
 };
 
-const contentChangeHandler = async (content) => {
+const contentChangeHandler = (content) => {
 	console.log("change", content);
 
 	// Skip if the content item is not published
@@ -48,26 +44,31 @@ const contentChangeHandler = async (content) => {
 		return;
 	}
 
-	// Skip if the content is not a page
-	if (!await isPage(content)) {
-		return;
-	}
+	return isPage(content).then((contentIsPage) => {
+		// Skip if the content is not a page
+		if (!contentIsPage) {
+			return;
+		}
 
-	const data = generateSitemapObjects(content);
+		const data = generateSitemapObjects(content);
 
-	console.log("send change data", data);
+		console.log("send change data", data);
+	});
 };
 
 const contentRemoveHandler = (content) => {
 	console.log("remove", content)
 
-	if (!await isPage(content)) {
-		return;
-	}
+	return isPage(content).then((contentIsPage) => {
+		// Skip if the content is not a page
+		if (!contentIsPage) {
+			return;
+		}
 
-	const data = generateSitemapObjects(content);
+		const data = generateSitemapObjects(content);
 
-	console.log("send remove data", data)
+		console.log("send remove data", data);
+	});
 };
 
 module.exports.start = () => {
